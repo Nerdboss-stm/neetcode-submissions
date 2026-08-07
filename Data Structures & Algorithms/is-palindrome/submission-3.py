f93fb54f3f893cd0f505add6d2654532cdc8940d
SARAN TEJA MALLELA <stmallela.us@gmail.com>
2026-08-07 19:10:41 -0400

    Add: is-palindrome - submission-3

---
+++ Data Structures & Algorithms/is-palindrome/submission-3.py
@@ -0,0 +1,17 @@
+import string
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        s1= s.lower()
+        s2 = s1.strip(" ")
+        s3 = s2.replace(" ","")
+        s4 = s3.translate(str.maketrans("", "", string.punctuation))
+        print(s4)
+        left = 0 
+        right = len(s4)-1
+        while left <= right:
+            if s4[left] != s4[right]:
+                return False
+            left += 1
+            right -= 1
+        return True
+        
